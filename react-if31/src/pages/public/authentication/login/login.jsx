18bc65528d384610c308b8318a061916fe27352d
angkyadi <angkyadiputra_2428250034@mhs.mdp.ac.id>
2025-12-01 19:47:08 +0700

    a

--- react-if31/src/pages/public/authentication/login/login.jsx
+++ react-if31/src/pages/public/authentication/login/login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
   };
  
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); 
     console.log("Form submitted:", form);
   };
 
